a8024e54b9fa42be37fba05a6241af34425dc7d3
AxelLedung <axel.ledung@gmail.com>
2023-10-31 14:46:15 +0100

    Completed tasks Lesson2

--- OP_Grund/Lesson_Training/Lesson2_Training/src/Main.java
+++ OP_Grund/Lesson_Training/Lesson2_Training/src/Main.java
@@ -1,10 +1,13 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+
         //Övning 1
         //Tillåt användaren att skriva in ett nummer mellan 1 och 10. Ifall de gör som du säger, beröm
         //dem. Säg att de gjort fel annars.
@@ -81,6 +84,7 @@ public class Main {
         //resultatet från 5*4*3*2*1. Testa att göra detta till en funktion som ger tillbaka numret
         System.out.println("Skriv in ett nummer:");
         int multiNum = scanner.nextInt();
+        scanner.nextLine();
         int sumNum = 1;
         int endNum = 0;
         for (int i = multiNum; i > 0; i--) {
@@ -92,24 +96,104 @@ public class Main {
         //redan har skrivit säger programmet till och ber en att skriva ett nytt i dess ställe. När alla är
         //nedskrivna så ge tillbaks dem.
 
-
-
-        //Har just nu feta logiska errors
         ArrayList<String> stringArrayList = new ArrayList<String>();
+        System.out.println("Skriv 5 Strängar:");
         for (int i = 0; i < 5; i++) {
             String fiveString = scanner.nextLine();
-            System.out.println("Skriv 5 Strängar:");
+            System.out.println("Du skrev in : " + fiveString);
             for (int j = 0; j < stringArrayList.size(); j++) {
                 if (stringArrayList.get(j).equals(fiveString)) {
-                    System.out.println("Finns redan en, ändra den tidigare med vad:");
-                    stringArrayList.add(j, scanner.nextLine());
+                    System.out.println("String finns redan, Vad ska den gamla heta? ");
+                    stringArrayList.set(j, scanner.nextLine());
+                    System.out.println("Nu kan du skriva nästa rad:");
                 }
             }
             stringArrayList.add(fiveString);
         }
-        /*for (int i = 0; i < stringArrayList.size(); i++) {
-            System.out.println(stringArrayList.get(i));
-        }*/
         System.out.println(stringArrayList);
+
+        //Uppgift 9
+        //Skriv ett program där användaren kan skriva ned så många nummer de vill tills de skriver
+        //”sluta”. Skriv sedan ned hur många unika nummer de skrev ned. Du vill nog kolla om
+        //användaren skrev ”sluta” innan du gör om vad de skrev till ett nummer.
+
+
+        //Uppgift 10
+        //Ha en funktion som heter Call som tar ett argument, ett nummer som är vem man ringer.
+        //Gör att funktionen ger tillbaka en string som är svaret från den man ringer, beroende på vem
+        //man ringer
+
+        //Funktionen skapad:
+        System.out.println(Call(1));
+        System.out.println(Call(2));
+        System.out.println(Call(3));
+        System.out.println(Call(4));
+        System.out.println(Call(95820952));
+
+
+        //Uppgift 11
+        //Vi skall spela Sten-Sax-Påse mot datorn. Slumpa först ut vad datorn kommer att göra, men
+        //spara det bara, visa det ej. Tillåt användaren att skriva in om de gör sten, sax eller påse. Om
+        //de skriver in någonting annat måste de skriva om. Visa nu vad datorn gjorde och bestäm
+        //vem som vann. Om det blev samma så kör man igen. Annars så vinner den vars val har
+        //övertaget. Sax slår påse, påse slår sten och sten slår sax. Använd loopar och if-satser.
+
+        System.out.println("Datorn vill spela Sten-Sax-Påse med dig!");
+        Random random = new Random();
+        int computerHand = random.nextInt(1, 4);
+        System.out.println("Vad väljer du?");
+        System.out.println("1. Sten");
+        System.out.println("2. Sax");
+        System.out.println("3. Påse");
+        int playerHand = scanner.nextInt();
+        scanner.nextLine();
+
+            System.out.println(computerHand);
+            if (computerHand == 1) {
+                System.out.println("Datorn valde: Sten");
+            } else if (computerHand == 2) {
+                System.out.println("Datorn valde: Sax");
+            } else if (computerHand == 3) {
+                System.out.println("Datorn valde: Påse");
+            } else {
+                System.out.println("VA?");
+            }
+            if (playerHand == 1) {
+                System.out.println("Du valde: Sten");
+            } else if (playerHand == 2) {
+                System.out.println("Du valde: Sax");
+            } else if (playerHand == 3) {
+                System.out.println("Du valde: Påse");
+            } else {
+                System.out.println("VA?");
+            }
+        if (playerHand == computerHand - 1) {
+            System.out.println("Du vann!");
+        }
+        else if (playerHand == 3 && computerHand == 1) {
+            System.out.println("Du vann!");
+        }
+        else if (playerHand == computerHand) {
+            System.out.println("Lika");
+            System.out.println(computerHand);
+            System.out.println(playerHand);
+        }
+        else {
+            System.out.println("Datorn vann!");
+        }
+    }
+    public static String Call(int num) {
+        switch (num) {
+            case 1:
+                return "Hej Son!";
+            case 2:
+                return "Jag har inte tid att prata nu.";
+            case 3:
+                return "Jag gillar glass";
+            case 4:
+                return "Hej pratar med Axel.";
+            default:
+                return "Numret du försöker nå är inte i bruk.";
+        }
     }
 }
