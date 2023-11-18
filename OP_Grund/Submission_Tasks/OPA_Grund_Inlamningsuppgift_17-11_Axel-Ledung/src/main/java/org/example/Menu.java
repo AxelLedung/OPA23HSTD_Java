dec8525909fde103a061664641a75a764cc94800
AxelLedung <axel.ledung@gmail.com>
2023-11-18 14:51:15 +0100

    RAF-MS PATCH 0.92.1 Added comment to Menu Class

--- OP_Grund/Submission_Tasks/OPA_Grund_Inlamningsuppgift_17-11_Axel-Ledung/src/main/java/org/example/Menu.java
+++ OP_Grund/Submission_Tasks/OPA_Grund_Inlamningsuppgift_17-11_Axel-Ledung/src/main/java/org/example/Menu.java
@@ -1,6 +1,8 @@
 package org.example;
 
 public class Menu {
+    //Since I wanted a nice GUI for RAF-MS but didn't want to write the header over and over again...
+    //I created the Class: Menu to add static functions such as Header()
     public static void Header(String message) {
         int width = 47;
         int padding = (width - message.length()) / 2;
